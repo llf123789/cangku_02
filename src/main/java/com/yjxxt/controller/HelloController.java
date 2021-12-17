e311f0cf0a4f40ffd55ffa3b8067ebe1f0cd0d0f
llf <llf_123789@qq.com>
2021-12-17 09:45:20 +0800

    提交项目到远程仓库

---
+++ src/main/java/com/yjxxt/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.yjxxt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HelloController {
+    /*** 请求映射地址 /hello.do * @return */
+    @RequestMapping("/hello")
+    public ModelAndView hello(){
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("hello", "hello spring mvc");
+        mv.setViewName("hello"); return mv;
+    }
+}
